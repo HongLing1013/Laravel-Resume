0e1d1179f7e02f74c6d386ed7f68cf8bed35bc4d
hollie1013 <hollie1013chao@gmail.com>
2023-06-30 12:53:33 +0800

    完成首頁文案的的grid頁面

--- app/Admin/Controllers/IndextextController.php
+++ app/Admin/Controllers/IndextextController.php
@@ -15,7 +15,7 @@ class IndextextController extends AdminController
      *
      * @var string
      */
-    protected $title = 'Indextext';
+    protected $title = '首頁文案';
 
     /**
      * Make a grid builder.
@@ -26,11 +26,30 @@ class IndextextController extends AdminController
     {
         $grid = new Grid(new Indextext());
 
-        $grid->column('id', __('Id'));
+        /* ============
+         * 禁用篩選按鈕
+         * 禁用導出按鈕
+         * 禁用行選擇器
+         * 禁用新增按鈕
+         * 禁用分頁功能
+         * 禁用删除功能
+         * ============ */
+        $grid->disableFilter();
+        $grid->disableExport();
+        $grid->disableRowSelector();
+        $grid->disableCreateButton();
+        $grid->disablePagination();
+        $grid->actions(function ($actions) {
+            $actions->disableDelete();
+        });
+
+        $grid->column('id', __('Id'))->hide();
         $grid->column('title', __('Title'));
         $grid->column('content', __('Content'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
+        $grid->column('created_at', __('創建時間'))->hide();
+        $grid->column('updated_at', __('更新時間'))->display(function ($updated_at) {
+            return date('Y-m-d H:i:s' , strtotime($updated_at));
+        });
 
         return $grid;
     }
