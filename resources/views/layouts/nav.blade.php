8653db0d13300f4511acf60474c353de5717e9cc
hollie1013 <hollie1013chao@gmail.com>
2023-07-11 13:16:58 +0800

    使用回圈把nav改成變數

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,11 +1,12 @@
 <nav id="navbar" class="navbar">
     <ul>
-        <li><a class="nav-link active" href="#header">Home</a></li>
-        <li><a class="nav-link" href="#about">About</a></li>
-        <li><a class="nav-link" href="#resume">Resume</a></li>
-        <li><a class="nav-link" href="#services">Services</a></li>
-        <li><a class="nav-link" href="#portfolio">Portfolio</a></li>
-        <li><a class="nav-link" href="#contact">Contact</a></li>
+        @foreach($menus as $menu)
+            @if($menu->title == 'home')
+                <li><a class="nav-link" href="#header">{{ ucfirst($menu->title) }}</a></li>
+            @else
+                <li><a class="nav-link" href="#{{ $menu->title }}">{{ ucfirst($menu->title) }}</a></li>
+            @endif
+        @endforeach
     </ul>
     <i class="bi bi-list mobile-nav-toggle"></i>
 </nav><!-- .navbar -->
