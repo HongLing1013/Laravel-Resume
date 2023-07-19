6ef4af23b19b7e9f732e96f487e5150819afe986
hollie1013 <hollie1013chao@gmail.com>
2023-07-19 13:47:30 +0800

    修改資料型態

---
+++ database/migrations/2023_07_19_054123_change_year_from_and_year_to_to_string_in_resume_experiences_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeYearFromAndYearToToStringInResumeExperiencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('resume_experiences', function (Blueprint $table) {
+            $table->string('year_from')->change();
+            $table->string('year_to')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('resume_experiences', function (Blueprint $table) {
+            $table->timestamp('year_from')->change();
+            $table->timestamp('year_to')->change();
+        });
+    }
+}
