d8f0f12797b2c9bf1aa9dedef6a32476717c0cd9
hollie1013 <tsubasababy1013@gmail.com>
2023-07-31 13:07:28 +0800

    完成工作經歷的控制器grid頁面

--- app/Admin/Controllers/ResumeExperienceController.php
+++ app/Admin/Controllers/ResumeExperienceController.php
@@ -38,8 +38,12 @@ class ResumeExperienceController extends AdminController
         $grid->column('year_from', __('開始時間'));
         $grid->column('year_to', __('結束時間'));
         $grid->column('company', __('工作地點'));
-        $grid->column('job_description', __('工作敘述'));
-        $grid->column('updated_at', __('更新時間'));
+        $grid->column('job_description', __('工作敘述'))->display(function ($job_description) {
+            return html_entity_decode(nl2br($job_description));
+        });
+        $grid->column('updated_at', __('更新時間'))->display(function($updated_at) {
+            return date("Y-m-d H:i:s" , strtotime($updated_at));
+        })->sortable();
 
         return $grid;
     }
@@ -73,7 +77,7 @@ class ResumeExperienceController extends AdminController
     {
         $form = new Form(new ResumeExperience());
 
-        $form->number('rcid', __('Rcid'))->hide();
+        $form->number('rcid', __('rcid'));
         $form->text('job_title', __('職稱'));
         $form->datetime('year_from', __('開始時間'))->default(date('Y-m-d H:i:s'));
         $form->datetime('year_to', __('結束時間'))->default(date('Y-m-d H:i:s'));
