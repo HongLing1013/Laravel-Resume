27854a128862c51ebe7d0f1ef952ea9c250319f1
hollie1013 <tsubasababy1013@gmail.com>
2023-07-31 13:12:29 +0800

    把空白的結束時間改成present

--- app/Admin/Controllers/ResumeExperienceController.php
+++ app/Admin/Controllers/ResumeExperienceController.php
@@ -36,7 +36,9 @@ class ResumeExperienceController extends AdminController
 
         $grid->column('job_title', __('職稱'));
         $grid->column('year_from', __('開始時間'))->sortable();
-        $grid->column('year_to', __('結束時間'))->sortable();
+        $grid->column('year_to', __('結束時間'))->display(function ($year_to){
+            return ($year_to != null) ?  date("Y-m" , strtotime($year_to)) :  'Present';
+        })->sortable();
         $grid->column('company', __('工作地點'));
         $grid->column('job_description', __('工作敘述'))->display(function ($job_description) {
             return html_entity_decode(nl2br($job_description));
