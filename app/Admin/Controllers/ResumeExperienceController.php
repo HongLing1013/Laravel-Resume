5386944daa8e0ae15feb21c044285ecd3c204a51
hollie1013 <tsubasababy1013@gmail.com>
2023-07-31 13:08:30 +0800

    加上排序

--- app/Admin/Controllers/ResumeExperienceController.php
+++ app/Admin/Controllers/ResumeExperienceController.php
@@ -35,8 +35,8 @@ class ResumeExperienceController extends AdminController
         $grid->disableColumnSelector();
 
         $grid->column('job_title', __('職稱'));
-        $grid->column('year_from', __('開始時間'));
-        $grid->column('year_to', __('結束時間'));
+        $grid->column('year_from', __('開始時間'))->sortable();
+        $grid->column('year_to', __('結束時間'))->sortable();
         $grid->column('company', __('工作地點'));
         $grid->column('job_description', __('工作敘述'))->display(function ($job_description) {
             return html_entity_decode(nl2br($job_description));
