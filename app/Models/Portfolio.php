67cfb7069e69c6325205a9d0bbff7aa399532886
hollie1013 <tsubasababy1013@gmail.com>
2023-08-04 13:32:37 +0800

    在model use 軟刪除功能並且把deleted_at加入可用字段

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Portfolio extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
 
-    protected $fillable = [ 'pcid' , 'text' , 'image' , 'href' ];
+    protected $fillable = [ 'pcid' , 'text' , 'image' , 'href' , 'deleted_at' ];
 
     public function portfolioCategory()
     {
