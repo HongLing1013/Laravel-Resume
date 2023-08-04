afe2be2d3024f7609309c42345f481b5199fb4ae
hollie1013 <tsubasababy1013@gmail.com>
2023-08-04 13:04:34 +0800

    完成resume experience的laravel-admin form頁面

--- app/Admin/Controllers/ResumeExperienceController.php
+++ app/Admin/Controllers/ResumeExperienceController.php
@@ -83,13 +83,51 @@ class ResumeExperienceController extends AdminController
 
         $form->text('job_title', __('職稱'));
         $form->datetime('year_from', __('開始時間'))->default(date('Y-m-d H:i:s'));
-        $form->datetime('year_to', __('結束時間'))->default(date('Y-m-d H:i:s'));
+        if($form->isCreating()){
+            $form->radioButton('over_time','結束時間')
+                 ->options([
+                     1 => '選擇時間' ,
+                     2 => '學習中',
+                 ])->when( 1, function (Form $form){
+                     $form->datetime('year_to', __(''))->default(date('Y-m-d H:i:s'));
+                 })->when( 2, function (Form $form){
+                     $form->radio('year_to', __(''))->value('');
+                 });
+         }else{
+             $path = explode('/', $_SERVER['REQUEST_URI'], -1);
+             if(isset($path[4]) && is_numeric($path[4])){
+                 $id = $path[4];
+                 $over_time = ResumeExperience::where('id',$id)->first();
+                 if($over_time){
+                     $year_to = $over_time->year_to ?? null;
+                     $over_time = $over_time->year_to == null ? 2 : 1;
+                     $form->radioButton('over_time','結束時間')
+                         ->options([
+                             1 => '選擇時間' ,
+                             2 => '學習中',
+                         ])->value($over_time)
+                         ->when( 1, function (Form $form) use($year_to){
+                             $form->datetime('year_to', __(''))->default(date('Y-m-d H:i:s'))->value($year_to);
+                         })->when( 2, function (Form $form){
+                             $form->radio('year_to', __(''))->value('');
+                         });
+                 }
+             }
+        }
         $form->text('company', __('工作地點'));
         $form->textarea('job_description', __('工作敘述'))->attribute(['id' => 'ckeditor']);;
 
         if($form->isCreating()){
             $form->hidden('rcid')->default('3');
         }
+        
+        $form->ignore(['over_time']);
+
+        $form->saved(function (Form $form){
+            $eduction = $form->model();
+            $year_to = $_POST['year_to'] ?? null;
+            ResumeExperience::where('id',$eduction->id)->update([ 'year_to'=>$year_to ]);
+        });
 
         return $form;
     }
