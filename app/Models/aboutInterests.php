d45fa3dc16ed187e5d01393469f266963f570a2d
hollie1013 <tsubasababy1013@gmail.com>
2023-08-12 14:55:20 +0800

    更改黨名

--- app/Models/aboutInterests.php
+++ app/Models/aboutInterests.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class aboutInterests extends Model
+class AboutInterests extends Model
 {
     use HasFactory;
 
