f419557e4b8bb202021d656360b274332b777f30
hollie1013 <tsubasababy1013@gmail.com>
2023-08-18 13:18:07 +0800

    完成about-interests的前台顯示

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -81,101 +81,37 @@
               </div>
           @endforeach
         </div>
-        
+
       </div><!-- End Skills -->
     @endif
 
-    {{-- @if($about->name == 'interests')
-      //
-    @endif --}}
+    @if($about->name == 'interests')
+      <!-- ======= Interests ======= -->
+      <div class="interests container">
+
+        <div class="section-title">
+          <h2>{{ $about->name }}</h2>
+        </div>
+
+        <div class="row">
+          @foreach($about->aboutInterests as $key => $interests)
+            <div class="col-lg-3 col-md-4 mt-4 @if($key <= 3) mt-md-0 @endif">
+              <div class="icon-box">
+                <i class="{{ $interests->icon }}" style="color: {{ $interests->color }}"></i>
+                <h3>{{ $interests->name }}</h3>
+              </div>
+            </div>
+          @endforeach
+        </div>
+
+      </div><!-- End Interests -->
+    @endif
+
     {{-- @if($about->name == 'testimonials')
       //
     @endif --}}
   @endforeach
 
-    <!-- ======= Interests ======= -->
-    <div class="interests container">
-
-      <div class="section-title">
-        <h2>Interests</h2>
-      </div>
-
-      <div class="row">
-        <div class="col-lg-3 col-md-4">
-          <div class="icon-box">
-            <i class="ri-store-line" style="color: #ffbb2c;"></i>
-            <h3>Lorem Ipsum</h3>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
-          <div class="icon-box">
-            <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
-            <h3>Dolor Sitema</h3>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
-          <div class="icon-box">
-            <i class="ri-calendar-todo-line" style="color: #e80368;"></i>
-            <h3>Sed perspiciatis</h3>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 mt-4 mt-lg-0">
-          <div class="icon-box">
-            <i class="ri-paint-brush-line" style="color: #e361ff;"></i>
-            <h3>Magni Dolores</h3>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 mt-4">
-          <div class="icon-box">
-            <i class="ri-database-2-line" style="color: #47aeff;"></i>
-            <h3>Nemo Enim</h3>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 mt-4">
-          <div class="icon-box">
-            <i class="ri-gradienter-line" style="color: #ffa76e;"></i>
-            <h3>Eiusmod Tempor</h3>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 mt-4">
-          <div class="icon-box">
-            <i class="ri-file-list-3-line" style="color: #11dbcf;"></i>
-            <h3>Midela Teren</h3>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 mt-4">
-          <div class="icon-box">
-            <i class="ri-price-tag-2-line" style="color: #4233ff;"></i>
-            <h3>Pira Neve</h3>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 mt-4">
-          <div class="icon-box">
-            <i class="ri-anchor-line" style="color: #b2904f;"></i>
-            <h3>Dirada Pack</h3>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 mt-4">
-          <div class="icon-box">
-            <i class="ri-disc-line" style="color: #b20969;"></i>
-            <h3>Moton Ideal</h3>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 mt-4">
-          <div class="icon-box">
-            <i class="ri-base-station-line" style="color: #ff5828;"></i>
-            <h3>Verdo Park</h3>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 mt-4">
-          <div class="icon-box">
-            <i class="ri-fingerprint-line" style="color: #29cc61;"></i>
-            <h3>Flavor Nivelanda</h3>
-          </div>
-        </div>
-      </div>
-
-    </div><!-- End Interests -->
 
     <!-- ======= Testimonials ======= -->
     <div class="testimonials container">
