8820de65e7a70496e927b3855ba017f5aae8e71f
hollie1013 <tsubasababy1013@gmail.com>
2023-08-22 12:56:34 +0800

    完成後台about count grid頁面

--- app/Admin/Controllers/AboutCountsController.php
+++ app/Admin/Controllers/AboutCountsController.php
@@ -26,11 +26,28 @@ class AboutCountsController extends AdminController
     {
         $grid = new Grid(new AboutCounts());
 
+        /* ============
+         * 禁用篩選按鈕
+         * 禁用導出按鈕
+         * 禁用行選擇器
+         * 禁用列選擇器
+         * 禁用分頁功能
+         * 禁用刪除按鈕
+         * ============ */
+
+        $grid->disableFilter();
+        $grid->disableExport();
+        $grid->disableRowSelector();
+        $grid->disableColumnSelector();
+        $grid->disablePagination();
+        $grid->actions(function (Grid\Displayers\Actions $actions) {
+           $actions->disableDelete();
+        });
+
         $grid->column('id', __('Id'))->hide();
-        $grid->column('name', __('好評分類'));
+        $grid->column('name', __('好評類別'));
         $grid->column('icon', __('Icon'));
         $grid->column('int', __('數量'));
-        $grid->column('created_at', __('創建時間'));
         $grid->column('updated_at', __('更新時間'));
 
         return $grid;
@@ -47,7 +64,7 @@ class AboutCountsController extends AdminController
         $show = new Show(AboutCounts::findOrFail($id));
 
         $show->field('id', __('Id'));
-        $show->field('name', __('好評分類'));
+        $show->field('name', __('好評類別'));
         $show->field('icon', __('Icon'));
         $show->field('int', __('數量'));
         $show->field('created_at', __('創建時間'));
@@ -65,7 +82,7 @@ class AboutCountsController extends AdminController
     {
         $form = new Form(new AboutCounts());
 
-        $form->text('name', __('好評分類'));
+        $form->text('name', __('好評類別'));
         $form->text('icon', __('Icon'));
         $form->text('int', __('數量'));
 
