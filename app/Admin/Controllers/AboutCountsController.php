97fa4335bf9f9b3d9a9d5985879adde291a2b0bb
hollie1013 <tsubasababy1013@gmail.com>
2023-08-22 12:51:31 +0800

    後about count翻成中文

--- app/Admin/Controllers/AboutCountsController.php
+++ app/Admin/Controllers/AboutCountsController.php
@@ -26,13 +26,12 @@ class AboutCountsController extends AdminController
     {
         $grid = new Grid(new AboutCounts());
 
-        $grid->column('id', __('Id'));
-        $grid->column('acid', __('Acid'));
-        $grid->column('name', __('Name'));
+        $grid->column('id', __('Id'))->hide();
+        $grid->column('name', __('好評分類'));
         $grid->column('icon', __('Icon'));
-        $grid->column('int', __('Int'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
+        $grid->column('int', __('數量'));
+        $grid->column('created_at', __('創建時間'));
+        $grid->column('updated_at', __('更新時間'));
 
         return $grid;
     }
@@ -48,12 +47,11 @@ class AboutCountsController extends AdminController
         $show = new Show(AboutCounts::findOrFail($id));
 
         $show->field('id', __('Id'));
-        $show->field('acid', __('Acid'));
-        $show->field('name', __('Name'));
+        $show->field('name', __('好評分類'));
         $show->field('icon', __('Icon'));
-        $show->field('int', __('Int'));
-        $show->field('created_at', __('Created at'));
-        $show->field('updated_at', __('Updated at'));
+        $show->field('int', __('數量'));
+        $show->field('created_at', __('創建時間'));
+        $show->field('updated_at', __('更新時間'));
 
         return $show;
     }
@@ -67,10 +65,13 @@ class AboutCountsController extends AdminController
     {
         $form = new Form(new AboutCounts());
 
-        $form->number('acid', __('Acid'));
-        $form->text('name', __('Name'));
+        $form->text('name', __('好評分類'));
         $form->text('icon', __('Icon'));
-        $form->text('int', __('Int'));
+        $form->text('int', __('數量'));
+
+        if($form->isCreating()){
+            $form->hidden('acid')->default('2');
+        }
 
         return $form;
     }
